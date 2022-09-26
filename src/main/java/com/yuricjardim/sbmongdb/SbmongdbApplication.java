f512bdd9fdf4f36e63703a74b4b2a91abed19b78
YuriCJardim <yuricjardim@gmail.com>
2022-09-26 08:59:38 -0300

    Projeto criado

---
+++ src/main/java/com/yuricjardim/sbmongdb/SbmongdbApplication.java
@@ -0,0 +1,13 @@
+package com.yuricjardim.sbmongdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbmongdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbmongdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yuricjardim/sbmongdb/SbmongdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuricjardim.sbmongdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbmongdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
